234ded4dd5b7314486f10b9653d06f3cc82cb9b4
DanArmstrong124 <danielarmstrong124@gmail.com>
2022-06-16 11:32:15 +0000

    Init

---
+++ run.py
@@ -0,0 +1,62 @@
+from TikTokLive import TikTokLiveClient
+from TikTokLive.types.events import CommentEvent, ConnectEvent
+
+# Instantiate the client with the user's username
+client: TikTokLiveClient = TikTokLiveClient(
+    unique_id="@danarms_x", **(
+        {
+            # Whether to process initial data (cached chats, etc.)
+            "process_initial_data": True,
+
+            # Connect info (viewers, stream status, etc.)
+            "fetch_room_info_on_connect": True,
+
+            # Whether to get extended gift info (Image URLs, etc.)
+            "enable_extended_gift_info": True,
+
+            # How frequently to poll Webcast API
+            "polling_interval_ms": 1000,
+
+            # Custom Client params
+            "client_params": {},
+
+            # Custom request headers
+            "headers": {},
+
+            # Custom timeout for Webcast API requests
+            "timeout_ms": 1000,
+
+            # Custom Asyncio event loop
+            "loop": None,
+
+            # Whether to trust environment variables that provide proxies to be used in aiohttp requests
+            "trust_env": False,
+
+            # A ProxyContainer object for proxied requests
+            "proxy_container": None,
+
+            # Set the language for Webcast responses (Changes extended_gift's language)
+            "lang": "en-US"
+
+        }
+    )
+)
+
+# Define how you want to handle specific events via decorator
+@client.on("connect")
+async def on_connect(_: ConnectEvent):
+    print("Connected to Room ID:", client.room_id)
+
+
+# Notice no decorator?
+async def on_comment(event: CommentEvent):
+    print(f"{event.user.nickname} -> {event.comment}")
+
+
+# Define handling an event via "callback"
+client.add_listener("comment", on_comment)
+
+if __name__ == '__main__':
+    # Run the client and block the main thread
+    # await client.start() to run non-blocking
+    client.run()
